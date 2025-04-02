92a5f33a83cb82b7fb24c27096bf4a07088e8b8d
Maf1sher <kubas307@gmail.com>
2025-04-02 22:26:55 +0200

    Initial commit

---
+++ together-backend/src/main/java/org/mafisher/togetherbackend/TogetherBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mafisher.togetherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TogetherBackendApplication.class, args);
+    }
+
+}
---
+++ together-backend/src/test/java/org/mafisher/togetherbackend/TogetherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mafisher.togetherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
