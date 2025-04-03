736dddc22e4442be774f4d2c24edc4dd6b47fd33
Maf1sher <kubas307@gmail.com>
2025-04-03 22:13:46 +0200

    Cookie service

---
+++ together-backend/src/main/java/org/mafisher/togetherbackend/service/CookieService.java
@@ -0,0 +1,10 @@
+package org.mafisher.togetherbackend.service;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface CookieService {
+    Cookie getNewCookie(String arg, String value);
+    Cookie deleteCookie(String arg);
+    String getJwtCookie(HttpServletRequest request);
+}
---
+++ together-backend/src/main/java/org/mafisher/togetherbackend/service/impl/CookieServiceImpl.java
@@ -0,0 +1,46 @@
+package org.mafisher.togetherbackend.service.impl;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.mafisher.togetherbackend.handler.BusinessErrorCodes;
+import org.mafisher.togetherbackend.handler.CustomException;
+import org.mafisher.togetherbackend.service.CookieService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CookieServiceImpl implements CookieService {
+    @Override
+    public Cookie getNewCookie(String arg, String value) {
+        Cookie cookie = new Cookie(arg, value);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(24 * 60 * 60);
+        return cookie;
+    }
+
+    @Override
+    public Cookie deleteCookie(String arg) {
+        Cookie cookie = new Cookie(arg, null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        return cookie;
+    }
+
+    @Override
+    public String getJwtCookie(HttpServletRequest request){
+        String jwt = "";
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("jwt")) {
+                    jwt = cookie.getValue();
+                    return jwt;
+                }
+            }
+        }
+        throw new CustomException(BusinessErrorCodes.BAD_COOKIE);
+    }
+}
