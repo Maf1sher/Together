195c824568053290eb1739282a772330ddb3382f
Maf1sher <kubas307@gmail.com>
2025-04-05 16:27:39 +0200

    HomePage

--- together-frontend/src/components/Navbar.jsx
+++ together-frontend/src/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import {useAuth} from "../providers/AuthProvider.jsx";
+import {logout} from "../api/authService.js";
 
 function NavBar() {
+
+    const {isAuthenticated, user, setUser, setAuthStatus} = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        setUser(null);
+        setAuthStatus(false)
+        navigate("/");
+    }
+
     return (
         <nav className="bg-gray-800 text-white px-6 py-4 shadow-md">
+
             <div className="max-w-7xl mx-auto flex justify-between items-center">
                 <Link to="/" className="text-2xl font-bold text-white hover:text-blue-400 transition">
                     MyApp
                 </Link>
+                {!isAuthenticated ?(
                 <div className="space-x-4">
                     <Link to="/login" className="hover:text-blue-400 transition">
                         Login
@@ -15,6 +30,20 @@ function NavBar() {
                         Register
                     </Link>
                 </div>
+                ): (
+                    <div className="space-x-4 flex">
+                        <div>
+                            {user.email}
+                        </div>
+
+                        <button
+                            onClick={handleLogout}
+                            className="font-bold text-white hover:text-blue-400 transition"
+                        >
+                            Logout
+                        </button>
+                    </div>
+                )}
             </div>
         </nav>
     );
